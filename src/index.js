328e15b92ce0784d8c35e8eb9702a44e5a9dbc6e
ksu14044 <mhm1404@naver.com>
2025-01-16 16:02:14 +0900

    ref 학습

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App5 from './study/App5';
 import App6 from './study/App6';
 import App7 from './study/App7';
 import App8 from './study/App8';
+import App9 from './study/App9';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -16,7 +17,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(<App3 />);
 //root.render(<App4 />);
 //root.render(<App7 />);
-root.render(<App8 />);
+// root.render(<App8 />);
+root.render(<App9 />);
 
 
 
